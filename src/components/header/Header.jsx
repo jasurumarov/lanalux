5a1b9188090e1950fe268ff3f1bbfc7f5edd5b61
jasurumarov <jasurumarov199@gmail.com>
2024-06-02 12:11:56 +0500

    header started

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
+// Images
+import SiteLogo from '../../assets/icons/site-logo.svg'
+
 const Header = () => {
   return (
     <header>
         <div className="container">
             <nav className="header__content">
-                
+              <img src={SiteLogo} alt="site logo" />
             </nav>
         </div>
     </header>
