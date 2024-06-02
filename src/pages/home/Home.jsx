0333b547998c561e89fe5fae464c6e16222cd0b0
QosimovRahmonjonn <qosimovrahmonjon2023@gmail.com>
2024-06-02 14:29:21 +0500

    wool-type ad

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,12 +3,15 @@ import React from "react";
 // Components
 import Hero from "../../components/hero/Hero";
 import Ads from "../../components/ads/Ads";
+import WoolType from "../../components/wool-type";
 
 const Home = () => {
   return (
     <main className="home-page">
       <Hero />
       <Ads />
+
+      <WoolType />
     </main>
   );
 };
