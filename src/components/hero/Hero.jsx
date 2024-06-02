4c72a59b9193cb91f68aaaf12b6559d40660c108
QosimovRahmonjonn <qosimovrahmonjon2023@gmail.com>
2024-06-02 12:57:12 +0500

    globall

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import bannerImg from "../../assets/images/banner.png";
+import ads from "../../assets/images/ads.png";
 
 const Hero = () => {
   return (
     <section className="hero-section">
       <div className="container">
         <div className="hero-section__content">
+          <div className="hero__text">
+            <h4>
+              Кардиганы: <br />
+              80% шерсти мериноса <br />и 20% шерсти ангоры
+            </h4>
+            <button>Перейти</button>
+          </div>
           <div className="hero__img">
             <img src={bannerImg} alt="Banner image" />
           </div>
         </div>
+      </div>
+      <div className="ads">
+        <img src={ads} alt="Advertising" />
       </div>
     </section>
   );
