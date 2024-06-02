b52177d373108768ad724f63397a19b7d5d8ef5f
jasurumarov <jasurumarov199@gmail.com>
2024-06-02 12:17:06 +0500

    react icons library added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { HiOutlineMenuAlt2 } from "react-icons/hi";
 
 // Images
 import SiteLogo from '../../assets/icons/site-logo.svg'
@@ -6,11 +7,15 @@ import SiteLogo from '../../assets/icons/site-logo.svg'
 const Header = () => {
   return (
     <header>
-        <div className="container">
-            <nav className="header__content">
-              <img src={SiteLogo} alt="site logo" />
-            </nav>
-        </div>
+      <div className="container">
+        <nav className="header__content">
+          <img src={SiteLogo} alt="site logo" />
+          <button>
+            <HiOutlineMenuAlt2   />
+            каталог
+          </button>
+        </nav>
+      </div>
     </header>
   )
 }
