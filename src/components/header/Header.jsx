9c440c7ed07b5d6e224c879054e89f102bd9c859
jasurumarov <jasurumarov199@gmail.com>
2024-06-02 12:58:10 +0500

    header continuing

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-import { HiOutlineMenuAlt2 } from "react-icons/hi";
+import { HiOutlineMenuAlt2, HiOutlineShoppingBag } from "react-icons/hi";
+import { Link } from 'react-router-dom';
 
 // Images
 import SiteLogo from '../../assets/icons/site-logo.svg'
+import UsIcon from '../../assets/icons/us.svg'
+import RuIcon from '../../assets/icons/ru.svg'
+import { GoSearch } from 'react-icons/go';
 
 const Header = () => {
   return (
@@ -10,10 +14,30 @@ const Header = () => {
       <div className="container">
         <nav className="header__content">
           <img src={SiteLogo} alt="site logo" />
-          <button>
-            <HiOutlineMenuAlt2   />
-            каталог
-          </button>
+          <div className='header__content-category'>
+            <button >
+              <HiOutlineMenuAlt2 />
+              каталог
+            </button>
+          </div>
+          <h3>321</h3>
+          <div className="header__content-contact">
+            <p>+7 (495) 508-76-45</p>
+            <Link>заказать звонок</Link>
+          </div>
+          <div className="header__content-btns">
+            <button className='header__content-search'>
+              <GoSearch />
+            </button>
+            <button className='header__content-cart'>
+              <HiOutlineShoppingBag />
+              <sup>10</sup>
+            </button>
+            <button className='header__content-lang'>
+              RU
+              <img src={RuIcon} alt="russia flag" />
+            </button>
+          </div>
         </nav>
       </div>
     </header>
