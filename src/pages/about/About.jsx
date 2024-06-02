8b598320e5add08218732217673c60464bbf941d
Jasur0919 <jasurjohn1111@gmail.com>
2024-06-02 14:26:10 +0500

    about-add

---
+++ src/pages/about/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AboutContent from "../../components/about-content/index"
+
+const About = () => {
+  return (
+    <>
+        <AboutContent/>
+    </>
+  )
+}
+
+export default About
