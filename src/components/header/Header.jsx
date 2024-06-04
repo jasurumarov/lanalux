764e7008ccace247785270da385265f490d5453b
jasurumarov <jasurumarov199@gmail.com>
2024-06-04 11:52:19 +0500

    swiper bu fixed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { HiChevronDown, HiOutlineMenuAlt2, HiOutlineShoppingBag } from "react-icons/hi";
 import { Link, useNavigate } from 'react-router-dom';
+import { useGetCategoryProductsQuery } from "../../context/api/productsApi";
 
 // Images
 import SiteLogo from '../../assets/icons/site-logo.svg'
@@ -9,8 +10,20 @@ import RuIcon from '../../assets/icons/ru.svg'
 import { GoSearch } from 'react-icons/go';
 import { HiArrowSmallDown } from 'react-icons/hi2';
 
+const categories = [
+  "Постельное бельё",
+  "Домашняя одежда",
+  "Одежда",
+  "Для детей",
+  "Подушки для сна",
+  "Декоративные подушки",
+  "Головные уборы и шарфы",
+  "Аксессуары и прочее"
+];
+
 const Header = () => {
   const [catalog, setCatalog] = useState(false)
+  const { data } = useGetCategoryProductsQuery();
   const navigate = useNavigate()
 
   return (
@@ -23,143 +36,48 @@ const Header = () => {
               <HiOutlineMenuAlt2 />
               <p>каталог</p>
             </button>
-            {
-              catalog ?
-                <div className="header__catalog-list">
-                  <ul className='header__catalog-list__content'>
-                    <li className='header__catalog-list__content-item'>
-                      Постельное бельё
+            {catalog && (
+              <div className="header__catalog-list">
+                <ul className='header__catalog-list__content'>
+                  {categories.map((category, index) => (
+                    <li key={index} className='header__catalog-list__content-item'>
+                      {category}
                       <HiArrowSmallDown />
                       <div>
                         <ul>
-                          <Link to={'/category'}>Наматрасники </Link>
-                          <Link to={'/category'}>Одеяла</Link>
-                          <Link to={'/category'}>Одеяла зимние</Link>
-                          <Link to={'/category'}>Подушки</Link>
-                          <Link to={'/category'}>Спальный комплект</Link>
-                        </ul>
-                      </div>
+                          {data?.slice(0, 5)?.map(el => (
+                            <Link key={el} to={`/category/${el}`} onClick={() => setCatalog(p => !p)}> { el }</Link>
+                          ))}
+                      </ul>
+                    </div>
                     </li>
-                    <li className='header__catalog-list__content-item'>
-                      Домашняя одежда
-                      <HiArrowSmallDown />
-                      <div>
-                        <ul>
-                          <Link onClick={() => setCatalog(p => !p)} to={'/category'}>Наматрасники </Link>
-                          <Link to={'/category'}>Одеяла</Link>
-                          <Link to={'/category'}>Одеяла зимние</Link>
-                          <Link to={'/category'}>Подушки</Link>
-                          <Link to={'/category'}>Спальный комплект</Link>
-                        </ul>
-                      </div>
-                    </li>
-                    <li className='header__catalog-list__content-item'>
-                      одежда
-                      <HiArrowSmallDown />
-                      <div>
-                        <ul>
-                          <Link to={'/category'}>Наматрасники </Link>
-                          <Link to={'/category'}>Одеяла</Link>
-                          <Link to={'/category'}>Одеяла зимние</Link>
-                          <Link to={'/category'}>Подушки</Link>
-                          <Link to={'/category'}>Спальный комплект</Link>
-                        </ul>
-                      </div>
-                    </li>
-                    <li className='header__catalog-list__content-item'>
-                      Для детей
-                      <HiArrowSmallDown />
-                      <div>
-                        <ul>
-                          <Link to={'/category'}>Наматрасники </Link>
-                          <Link to={'/category'}>Одеяла</Link>
-                          <Link to={'/category'}>Одеяла зимние</Link>
-                          <Link to={'/category'}>Подушки</Link>
-                          <Link to={'/category'}>Спальный комплект</Link>
-                        </ul>
-                      </div>
-                    </li>
-                    <li className='header__catalog-list__content-item'>
-                      Подушки для сна
-                      <HiArrowSmallDown />
-                      <div>
-                        <ul>
-                          <Link to={'/category'}>Наматрасники </Link>
-                          <Link to={'/category'}>Одеяла</Link>
-                          <Link to={'/category'}>Одеяла зимние</Link>
-                          <Link to={'/category'}>Подушки</Link>
-                          <Link to={'/category'}>Спальный комплект</Link>
-                        </ul>
-                      </div>
-                    </li>
-                    <li className='header__catalog-list__content-item'>
-                      Декоративные подушки
-                      <HiArrowSmallDown />
-                      <div>
-                        <ul>
-                          <Link to={'/category'}>Наматрасники </Link>
-                          <Link to={'/category'}>Одеяла</Link>
-                          <Link to={'/category'}>Одеяла зимние</Link>
-                          <Link to={'/category'}>Подушки</Link>
-                          <Link to={'/category'}>Спальный комплект</Link>
-                        </ul>
-                      </div>
-                    </li>
-                    <li className='header__catalog-list__content-item'>
-                      Головные уборы
-                      и шарфы
-                      <HiArrowSmallDown />
-                      <div>
-                        <ul>
-                          <Link to={'/category'}>Наматрасники </Link>
-                          <Link to={'/category'}>Одеяла</Link>
-                          <Link to={'/category'}>Одеяла зимние</Link>
-                          <Link to={'/category'}>Подушки</Link>
-                          <Link to={'/category'}>Спальный комплект</Link>
-                        </ul>
-                      </div>
-                    </li>
-                    <li className='header__catalog-list__content-item'>
-                      Аксессуары и прочее
-                      <HiArrowSmallDown />
-                      <div>
-                        <ul>
-                          <Link to={'/category'}>Наматрасники </Link>
-                          <Link to={'/category'}>Одеяла</Link>
-                          <Link to={'/category'}>Одеяла зимние</Link>
-                          <Link to={'/category'}>Подушки</Link>
-                          <Link to={'/category'}>Спальный комплект</Link>
-                        </ul>
-                      </div>
-                    </li>
-                  </ul>
-                </div>
-                : <></>
-
-            }
-          </div>
-          <h3>Одежда и постель из натуральной шерсти</h3>
-          <div className="header__content-contact">
-            <p>+7 (495) 508-76-45</p>
-            <Link to={'tel: +7 495 508-76-45'}>заказать звонок</Link>
-          </div>
-          <div className="header__content-btns">
-            <button className='header__content-search'>
-              <GoSearch />
-            </button>
-            <button onClick={() => navigate('/cart')} className='header__content-cart'>
-              <HiOutlineShoppingBag />
-              <sup>10</sup>
-            </button>
-            <button className='header__content-lang'>
-              RU
-              <img src={RuIcon} alt="russia flag" />
-              <HiChevronDown />
-            </button>
-          </div>
-        </nav>
+                  ))}
+              </ul>
+              </div>
+            )}
       </div>
-    </header>
+      <h3>Одежда и постель из натуральной шерсти</h3>
+      <div className="header__content-contact">
+        <p>+7 (495) 508-76-45</p>
+        <Link to={'tel: +7 495 508-76-45'}>заказать звонок</Link>
+      </div>
+      <div className="header__content-btns">
+        <button className='header__content-search'>
+          <GoSearch />
+        </button>
+        <button onClick={() => navigate('/cart')} className='header__content-cart'>
+          <HiOutlineShoppingBag />
+          <sup>10</sup>
+        </button>
+        <button className='header__content-lang'>
+          RU
+          <img src={RuIcon} alt="russia flag" />
+          <HiChevronDown />
+        </button>
+      </div>
+    </nav>
+      </div >
+    </header >
   )
 }
 
